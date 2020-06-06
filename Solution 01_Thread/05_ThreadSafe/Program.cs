b469a473fa369b2ee0241c330399e71ff5ebf114
luiz marcello <luizmarcello.vmo@hotmail.com>
2020-06-06 12:59:10 -0300

    Algumas alterações 5

--- Solution 01_Thread/05_ThreadSafe/Program.cs
+++ Solution 01_Thread/05_ThreadSafe/Program.cs
@@ -5,22 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 
-namespace _04_ThreadBackground
+namespace _05_ThreadSafe
 {
     class Program
     {
+        static int rede = 0;
+        static object variavelDeControle = 0;
+
+        //IO - Imput/Output Lento(tela, Rede, Armazenamento, impressora-rede)
         static void Main(string[] args)
         {
             Console.WriteLine("DataIni: " + DateTime.Now);
 
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < 5; i++)
             {
                 Thread t1 = new Thread(ThreadRepeticao);
                 //Esta propriedade fará com que as threads fiquem totalmente
                 //dependentes do fluxo principal, do "static void main".
                 //Quando ele está executando, eleas continuam, quando ele
                 //para, elas também param. Muito bom!
-                t1.IsBackground = true;                                   
+                t1.IsBackground = true;
                 t1.Start();
             }
             Console.ReadKey();
@@ -31,11 +35,20 @@ namespace _04_ThreadBackground
         //Este método vai ser executado no "fluxo paralelo".
         static void ThreadRepeticao()
         {
-            for (int i = 0; i < 3000; i++)
+            //FIFO - First in first out
+            for (int i = 0; i < 1000; i++)
             {
-                Console.WriteLine("Fluxo paraleleo: " + i);
+                lock (variavelDeControle)
+                {
+                    Console.WriteLine("Fluxo paraleleo: " + i);
+                    rede++;
+                }
             }
+
+
             Console.WriteLine("DataIni: " + DateTime.Now);
         }
+
     }
 }
+
